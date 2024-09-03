d1c6ad9a6caf6e3d0f2625887d2a01fbda0fbacb
hasoro <noily4748@gmail.com>
2024-09-03 11:36:04 +0900

    09-03.11:00

---
+++ ch03/src/main/java/com/ch03/MyAdvice.java
@@ -0,0 +1,44 @@
+package com.ch03;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyAdvice {
+
+    //포인트컷
+    @Pointcut("execution(void com.ch03.MyService.insert())")
+    public void insertPointcut(){} //핵심관심을 가리키는 내용이 없는 메서드
+
+    @Pointcut("execution(void com.ch03.MyService.select(..))")
+    public void selectPointcut(){}
+
+    @Before("insertPointcut() || selectPointcut()")
+    public void beforeAdvice() {
+        System.out.println("부가기능 - beforeAdvice...");
+    }
+
+    @After("insertPointcut()")
+    public void afterAdvice() {
+        System.out.println("부가기능 - afterAdvice...");
+    }
+
+    @AfterReturning("insertPointcut()")
+    public void afterReturningAdvice() {
+        System.out.println("부가기능 - afterReturningAdvice...");
+    }
+
+    @Around("insertPointcut()")
+    public void aroundAdvice(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("부가기능 - aroundAdvice1...");
+        pjp.proceed();  // 핵심 기능 실행
+        System.out.println("부가기능 - aroundAdvice2...");
+    }
+
+    @AfterThrowing("selectPointcut()")
+    public void aroundThrowingAdvice() {
+        System.out.println("부가기능 - aroundThrowingAdvice...");
+    }
+}
---
+++ ch04/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+    <!--
+        날짜 : 2024/09/03
+        이름 : 강중원
+        내용 : 4장 Spring MVC 실습하기
+    -->
+</head>
+<body>
+<h2>4장 Spring MVC </h2>
+</body>
+</html>
